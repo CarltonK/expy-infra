129f3cb1745140b386f2a32bdaa19940a9dbed1b
Mark Carlton <mcarlton33@gmail.com>
2021-06-26 21:51:22 +0300

    add cloud run and connection to sql

--- cloud_run.tf
+++ cloud_run.tf
@@ -1,29 +1,30 @@
-# resource "google_cloud_run_service" "default" {
-#   name     = "cloudrun-srv"
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "europe-west3"
 
-#   template {
-#     spec {
-#       containers {
-#         image = "us-docker.pkg.dev/cloudrun/container/hello"
-#       }
-#     }
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
 
-#     metadata {
-#       annotations = {
-#         "autoscaling.knative.dev/maxScale"      = "1000"
-#         "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.instance.connection_name
-#         "run.googleapis.com/client-name"        = "terraform"
-#       }
-#     }
-#   }
-#   autogenerate_revision_name = true
-# }
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale"      = "1000"
+        "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.instance.connection_name
+        "run.googleapis.com/client-name"        = "terraform"
+      }
+    }
+  }
+  autogenerate_revision_name = true
+}
 
-# resource "google_sql_database_instance" "instance" {
-#   name   = "cloudrun-sql"
-#   settings {
-#     tier = "db-f1-micro"
-#   }
+resource "google_sql_database_instance" "instance" {
+  name   = "cloudrun-sql"
+  settings {
+    tier = "db-f1-micro"
+  }
 
-#   deletion_protection  = "true"
-# }
+  deletion_protection  = "true"
+}
