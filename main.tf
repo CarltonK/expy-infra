e35fdccc0ba64fda8a2c3146df8289b9ae3e68a1
Mark Carlton <mcarlton33@gmail.com>
2021-06-26 20:42:02 +0300

    change project

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
   credentials = file("CREDENTIALS_FILE.json")
-  project     = "nijenge"
+  project     = "expy-317620"
   region      = "europe-west3"
   zone        = "europe-west3-a"
 }
