f9864729730108c2b1af9a04aeafc48adc6ae1d7
Mark Carlton <mcarlton33@gmail.com>
2021-09-27 07:06:37 +0300

    provider lock

--- main.tf
+++ main.tf
@@ -5,18 +5,22 @@ terraform {
       source  = "hashicorp/google"
       version = "3.83.0"
     }
-  }
-  # backend "remote" {
-  #   organization = var.org_name
 
-  #   workspaces {
-  #     name = var.workspace_id
-  #   }
-  # }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "3.85.0"
+    }
+  }
+  backend "remote" {
+    organization = var.org_name
+
+    workspaces {
+      name = var.workspace_id
+    }
+  }
 }
 
 provider "google" {
-  # Needed for CI pipeline
   credentials = file("CREDENTIALS_FILE.json")
   project     = var.project_id
   region      = var.location_id
@@ -26,4 +30,6 @@ provider "google" {
 provider "google-beta" {
   project = var.project_id
   credentials = file("CREDENTIALS_FILE.json")
+  region      = var.location_id
+  zone        = var.zone_id
 }
