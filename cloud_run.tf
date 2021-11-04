d023b8646ebd4d8b91894b21676fab315da35bad
Mark Carlton <mcarlton33@gmail.com>
2021-11-04 09:36:02 +0300

    Auto stash before merge of "develop" and "origin/develop"

--- cloud_run.tf
+++ cloud_run.tf
@@ -14,6 +14,10 @@ resource "google_cloud_run_service" "expy-dev" {
           name  = "NODE_ENV"
           value = var.workspace_env
         }
+        env {
+          name = "ACCESS_TOKEN"
+          value = var.database_password
+        }
       }
 
       service_account_name = "${var.project_name}-${var.workspace_env}-591@${var.project_id}.iam.gserviceaccount.com"
@@ -39,4 +43,27 @@ resource "google_cloud_run_service_iam_member" "allUsers" {
   location = google_cloud_run_service.expy-dev.location
   role     = "roles/run.invoker"
   member   = "allUsers"
+}
+
+# Cloud Build
+resource "google_cloudbuild_trigger" "build-trigger" {
+  name = "${var.workspace_env}-cloud-run-builder"
+  tags = [ var.workspace_env ]
+
+  github {
+    owner = "CarltonK"
+    name  = "expense_manager"
+    push {
+      branch = "develop"
+    }
+  }
+
+  substitutions = {
+    _CONNECTION_NAME = google_sql_database_instance.instance.connection_name
+    _DATABASE_URL    = "postgres://${var.database_user}:${var.database_password}@localhost/${google_sql_database_instance.instance.name}?host=/cloudsql/${google_sql_database_instance.instance.connection_name}"
+    _PROJECT_REGION  = var.location_id
+    _PROJECT_SERVICE = "${var.project_name}-${var.workspace_env}"
+  }
+
+  filename = "cloudbuild/${var.workspace_env}.yaml"
 }
