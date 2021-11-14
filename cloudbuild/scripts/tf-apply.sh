5e30b0bfabd0305788e176e5b9c9a9666f7586d3
Mark Carlton <mcarlton33@gmail.com>
2021-11-14 09:52:18 +0300

    automatic destruction

--- cloudbuild/scripts/tf-apply.sh
+++ cloudbuild/scripts/tf-apply.sh
@@ -1,6 +1,6 @@
 if [ -d "environments/$1/" ]; then
     cd environments/$1      
-    terraform destroy
+    terraform destroy -auto-approve
 else
     echo "***************************** SKIPPING APPLYING *******************************"
     echo "Branch '$1' does not represent an official environment."
